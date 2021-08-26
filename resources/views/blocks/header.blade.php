7085bbd477c41bea489099217d00b4cf2a6faa64
Andrew kodeksoff@gmail.com <kodeksoff@gmail.com>
2021-08-26 17:40:56 +0300

    init

---
+++ resources/views/blocks/header.blade.php
@@ -0,0 +1,54 @@
+<header>
+    <nav class="header-nav">
+        <div class="container">
+            <div class="flex justify-between items-center">
+                <div class="header-nav__burger sm:block md:hidden">
+                    <div class="burger-button">
+                        <img src="img/burger-button.svg" alt="Menu">
+                    </div>
+                </div>
+                <div class="header-nav__left flex items-center">
+                    <div class="header-nav__logo">
+                        <a href="/"><img src="img/logo.svg" alt="Maxmoll"></a>
+                    </div>
+                    <div class="header-nav__navigation hidden md:block">
+                        <ul class="flex">
+                            <li><a href="/">Условия работы</a></li>
+                            <li><a href="/">Обязанности и требования</a></li>
+                            <li><a href="/">О «Maxmoll»</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="header-nav__right">
+                    <button class="button hidden xl:block">Заполнить анкету</button>
+                    <button class="button sm:block xl:hidden">Анкета</button>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <div class="first-screen flex flex-col justify-between">
+        <div class="container">
+            <div class="flex flex-col md:flex-row items-center justify-between">
+                <div class="first-screen-left">
+                    <div class="first-screen-left__content">
+                        <div class="first-screen-left__title font-custom">Есть работа!</div>
+                        <div class="first-screen-left__subtitle font-custom">Кладовщика комплектовщик
+                            (п.Коммунарка)
+                        </div>
+                        <div class="first-screen-left__options flex flex-col-reverse md:flex-row items-center mt-0 md:mt-6 ">
+                            <button class="button button-with-arrow mx-auto md:mx-0 mt-3 md:mt-0 mb-9 md:mb-0">Анкета</button>
+                            <div class="italic font-weight-bold md:ml-7 ml-0">Присоединяйтесь к нашей команде уже сейчас
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="first-screen-right">
+                    <div class="first-screen-right__content">
+                        <img src="img/first-screen-main.png" alt="main-image">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">поселок Коммунарка, улица <br /> Александры Монаховой, 57, вл2</div>
+    </div>
+</header>
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+    purge: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+    ],
+
+    theme: {
+        container: {
+            center: true,
+            padding: '1rem',
+
+        },
+        screens: {
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+            '2xl': '1760px',
+        },
+        extend: {},
+    },
+
+    variants: {
+        extend: {},
+    },
+
+    plugins: [require('@tailwindcss/forms')],
+};
